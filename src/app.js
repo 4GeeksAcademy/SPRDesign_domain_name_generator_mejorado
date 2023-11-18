ae49f946396c57c5121fa3500d75fa7a0c0bc4de
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2023-11-18 18:42:33 +0000

    domaingenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function domainGenerate(pronouns, adjs, nouns, extensions) {
+    let myArray = [];
+
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjs.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let l = 0; l < extensions.length; l++) {
+            let resultPronoun = pronouns[i];
+            let resultAdj = adjs[j];
+            let resultNoun = nouns[k];
+            let resultExtension = extensions[l];
+
+            let domain =
+              resultPronoun + resultAdj + resultNoun + resultExtension;
+
+            myArray.push(domain);
+          }
+        }
+      }
+    }
+    return myArray;
+  }
+
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".es"];
+
+  let result = domainGenerate(pronouns, adjs, nouns, extensions);
+  let olElement = document.querySelector("#domainGenerator ol");
+
+  result.forEach(function(domain) {
+    let liElement = document.createElement("li");
+    liElement.textContent = domain;
+    olElement.appendChild(liElement);
+  });
 };
